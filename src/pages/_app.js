6682fdfd41b5c10d51370cf86ff942c06cad8f67
ilhambara <g2a.ilhammubarok@gmail.com>
2022-07-28 13:54:12 +0700

    update: install chakra ui + modify home page

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import { ChakraProvider } from "@chakra-ui/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
